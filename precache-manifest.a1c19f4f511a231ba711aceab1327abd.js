e4c7717229aa83cb65765d02f198eccb02ee29e4
isuru <isuru.udayanga@ayra.com.au>
2020-05-03 11:21:22 +0530

    Updates

---
+++ precache-manifest.a1c19f4f511a231ba711aceab1327abd.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9631c99bd326c5367db48e4d5197724b",
+    "url": "/react-board-game/index.html"
+  },
+  {
+    "revision": "fc29e371c6256f8db73b",
+    "url": "/react-board-game/static/css/main.bb4d29d5.chunk.css"
+  },
+  {
+    "revision": "612dd6620d934b2e24e0",
+    "url": "/react-board-game/static/js/2.0c17a115.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/react-board-game/static/js/2.0c17a115.chunk.js.LICENSE"
+  },
+  {
+    "revision": "fc29e371c6256f8db73b",
+    "url": "/react-board-game/static/js/main.cedf8645.chunk.js"
+  },
+  {
+    "revision": "2c18d700f9679c3a1a9c",
+    "url": "/react-board-game/static/js/runtime-main.37c8e197.js"
+  }
+]);
